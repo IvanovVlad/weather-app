2cefee82f11883abfe85e32421253023807f103e
IvanovVlad <rr.rr0@mail.ru>
2020-02-08 01:20:22 +0400

    feat: added part of rendering logic

--- src/js/form.js
+++ src/js/form.js
@@ -1,4 +1,14 @@
 import { WeatherApi } from './WeatherApi';
 import { WeatherData } from './WeatherData';
 
-WeatherApi.getCurrentWeather('Izhevsk').then(data => console.log(new WeatherData(data)));
+WeatherApi.getCurrentWeather('Izhevsk').then(data => {
+    renderPage(new WeatherData(data));
+});
+
+function renderPage(weatherData) {
+    document.querySelector('#weather-now').innerText = weatherData.weather.temp + '°';
+    document.querySelector('#feels-now').innerText = weatherData.weather.feelsTemp + '°';
+    document.querySelector('#environment-now').innerText = weatherData.weather.description;
+    document.querySelector('#wind-now').innerText = weatherData.weather.windSpeed + ' m/s';
+    document.querySelector('#humidity-now').innerText = weatherData.weather.humidity + '%';
+}
