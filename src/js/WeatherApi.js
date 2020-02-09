af759bcbe22667ea3b80690d5423515af70f8193
IvanovVlad <rr.rr0@mail.ru>
2020-02-09 15:13:41 +0400

    feat: added error handler for weatherApi

--- src/js/WeatherApi.js
+++ src/js/WeatherApi.js
@@ -5,16 +5,24 @@ export class WeatherApi {
     constructor() { }
 
     static async getCurrentWeather(city) {
-        const response = await fetch(`https://api.weatherbit.io/v2.0/current?city=${city}&key=${WeatherApi.key}`);
-        const json = await response.json();
-        const data = json.data[0];
-        return data;
+        try {
+            const response = await fetch(`https://api.weatherbit.io/v2.0/current?city=${city}&key=${WeatherApi.key}`);
+            const json = await response.json();
+            const data = json.data[0];
+            return data;
+        } catch (e) {
+            alert(e);
+        }
     }
 
     static async getForecastWeather(city) {
-        const response = await fetch(
-            `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${WeatherApi.key}`);
-        const json = await response.json();
-        return json.data;
+        try {
+            const response = await fetch(
+                `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${WeatherApi.key}`);
+            const json = await response.json();
+            return json.data;
+        } catch (e) {
+            alert(e);
+        }
     }
 }
