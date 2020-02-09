b3a155ab2e7d9db8b137758e27a9c508698b0cec
IvanovVlad <rr.rr0@mail.ru>
2020-02-10 00:48:02 +0400

    fix: removed unused code

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -4,12 +4,6 @@ preloader.id = 'preloader';
 
 document.body.appendChild(preloader)
 
-document.addEventListener('readystatechange', () => {
-    if(document.readyState == 'complete') {
-        destroyPreloader();
-    }
-});
-
 export function hidePreloader(){
     preloader.className = 'hidden';
 };
