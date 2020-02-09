9ebdaf27cc8974c5411a8a4bf7cd63e0612c6aae
IvanovVlad <rr.rr0@mail.ru>
2020-02-09 18:30:53 +0400

    feat: added search on enter after input function

--- src/js/form.js
+++ src/js/form.js
@@ -5,6 +5,12 @@ import { renderImage } from './imageApi';
 document.querySelector('#search-button').onclick =
     () => renderPage(document.querySelector('#search-field').value);
 
+document.querySelector('#search-field').onkeypress = (e) => {
+    if (e.key === 'Enter') {
+       renderPage(e.target.value);
+    }
+}
+
 getLocation().then(loc => renderPage(loc));
 
 document.querySelector('#refresh').onclick = renderImage;
