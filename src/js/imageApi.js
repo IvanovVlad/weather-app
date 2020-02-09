ca684e32958d43ba432a5e4457bcdaab1de45e02
IvanovVlad <rr.rr0@mail.ru>
2020-02-09 18:02:49 +0400

    feat: background looks a little better now

--- src/js/imageApi.js
+++ src/js/imageApi.js
@@ -1,7 +1,7 @@
 const imageAPIKey = '563492ad6f91700001000001e5099a744d6d4faba7664ce972e647cb';
 
 function getImage(keyWords) {
-    return fetch(`https://api.pexels.com/v1/search?query=${keyWords.toString()}&per_page=10&page=1`, {
+    return fetch(`https://api.pexels.com/v1/search?query=${keyWords.toString()}&per_page=20&page=1`, {
         headers: {
             'Authorization': imageAPIKey
         }
@@ -20,12 +20,11 @@ export function renderImage() {
 
     getImage(weatherDescription).then(img => {
         const image = new Image();
-        const photoJSON = img.photos[getRandomInt(10)];
-        image.src = photoJSON.src.portrait;
+        const photoJSON = img.photos[getRandomInt(20)];
+        image.src = photoJSON.src.landscape;
         image.onload = function () {
             const background = document.querySelector('.background');
             background.style.backgroundImage = `url('${this.src}')`;
-            background.style.backgroundSize = 'cover';
             background.innerText = "@" + photoJSON.photographer;
         };
     });
